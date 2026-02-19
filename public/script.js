f7e1278f330a985fb63e4fedf99915c8c3abac1c
Moritz Wendt <moritz_wendt@icloud.com>
2026-02-19 18:34:44 +0100

    emailjs

--- public/script.js
+++ public/script.js
@@ -175,3 +175,36 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
 });
+// EmailJS ###############
+(function () {
+    emailjs.init("-FGnRHyKguim-btDt");
+})();
+
+const contactForm = document.getElementById('contactForm');
+const successMessage = document.getElementById('success-message');
+
+contactForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const formData = {
+        to_name: "Admin",
+        from_name: document.getElementById('name').value,
+        from_email: document.getElementById('email').value,
+        subject: document.getElementById('subject').value,
+        message: document.getElementById('message').value,
+        reply_to: document.getElementById('email').value 
+    };
+
+    emailjs.send('service_gbcu138', 'template_m76o0ua', formData).then(function (response) {
+
+        // successMessage.classList.remove('hidden');
+
+        // contactForm.reset();
+
+        // setTimeout(() => {
+        //     successMessage.classList.add('hidden');
+        // }, 5000);
+    }).catch(function (error) {
+        alert('Failed to send message. Please try again.');
+    });
+});
